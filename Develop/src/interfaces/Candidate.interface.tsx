431ecfb61e743ee7f66a293364ce622aa37fe750
Robby <robby@henderson-woods.com>
2024-11-30 05:12:04 -0600

    started to work on pages and components

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    login: string;
+    avatar_url: string;
+    html_url: string;
+    location: string;
+    bio: string;
+    email: string;
+    company: string;
+}
